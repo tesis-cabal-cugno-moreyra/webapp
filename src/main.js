66eeef607b7a2622746c5e58cf9b9b7a28657869
Gnoq <emi.cugno@gmail.com>
2020-08-24 17:58:35 -0300

    Add API service, Axios and VueAxios

--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import vuetify from "./plugins/vuetify";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 
+import api from "./services/api";
+import VueAxios from "vue-axios";
+import axios from "axios";
+
+Vue.router = router;
+Vue.use(VueAxios, axios);
+Vue.use(api);
+Vue.axios.defaults.baseURL = "http://localhost:8000/api/v1";
+
 Vue.config.productionTip = false;
 
 new Vue({
