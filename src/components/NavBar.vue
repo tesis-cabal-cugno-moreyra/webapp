0e69505dc5e8333b5e265a1ccf275736e7a00924
Joa Cabal <cabaljoa@gmail.com>
2020-08-31 20:33:29 -0300

    NavBar Created

---
+++ src/components/NavBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-navigation-drawer v-model="drawer" app clipped temporary>
+      <v-layout mt-4 column align-center>
+        <v-flex>
+          <v-avatar>
+            <v-img
+              src="https://randomuser.me/api/portraits/women/85.jpg"
+            ></v-img>
+          </v-avatar>
+        </v-flex>
+        <v-flex>
+          <p class="gray--text mt-3 headline">Facundito</p>
+        </v-flex>
+      </v-layout>
+      <v-divider></v-divider>
+
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon color="grey darken-1">mdi-settings</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title class="grey--text text--darken-1"
+          >Configuracion</v-list-item-title
+        >
+      </v-list-item>
+
+      <v-switch
+        class="ml-4"
+        v-model="$vuetify.theme.dark"
+        inset
+        :label="
+          `${
+            $vuetify.theme.dark
+              ? 'Cambiar a tema claro'
+              : 'Cambiar a tema oscuro'
+          }  `
+        "
+      ></v-switch>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Sicoin</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <!--  <v-btn class="success" @click="accountDialog = 'true'">Ingresar</v-btn>-->
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data: () => ({
+    drawer: false
+  })
+};
+</script>
