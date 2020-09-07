c1e81d7155ba279dc5c2f050cd7fec3b4e294f5b
Gnoq <emi.cugno@gmail.com>
2020-09-07 19:16:30 -0300

    Add Login view

---
+++ src/views/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Iniciar sesión</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    id="user"
+                    v-model="user"
+                    label="Usuario"
+                    name="user"
+                    prepend-icon="mdi-account"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    v-model="password"
+                    label="Contraseña"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" :loading="tryToLogin" v-on:click="login"
+                  >Ingresar</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data: function() {
+    return {
+      user: "",
+      password: "",
+      tryToLogin: false
+    };
+  },
+  methods: {
+    login: function() {}
+  }
+};
+</script>
