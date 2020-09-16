27d013ebd8c1c51299e793c4026da0a72a458db0
Joa Cabal <cabaljoa@gmail.com>
2020-09-16 15:23:04 -0300

    wip Steps 4 working

--- src/components/DomainInformation.vue
+++ src/components/DomainInformation.vue
@@ -84,34 +84,42 @@
         <v-row>
           <v-col>
             <v-card sm="6" md="4" width="400" left>
-              <v-card-subtitle>Supervisores actuales</v-card-subtitle>
-              <v-card-text
-                :class="['pa-2', 'black_selected']"
-                sm="6"
-                md="4"
-                v-for="(item, index) in itemsSupervisorList"
-                :key="index"
+              <v-card-subtitle
+                v-if="itemsSupervisorList.length == 0 ? true : false"
+                >No hay cargados supervisores actualmente</v-card-subtitle
               >
-                {{ item.name }}
-                <v-btn
-                  icon
-                  color="success"
-                  class="['mr-2','float-right']"
-                  small
-                  @click="changeFormSupervisor(index)"
-                >
-                  <v-icon>mdi-pencil</v-icon>
-                </v-btn>
-                <v-btn
-                  icon
-                  color="primary"
-                  small
-                  @click="deleteSupervisor(item.id)"
-                >
-                  <v-icon>mdi-delete</v-icon>
-                </v-btn>
-              </v-card-text>
             </v-card>
+            <v-form v-if="itemsSupervisorList.length != 0 ? true : false">
+              <v-card sm="6" md="4" width="400" left>
+                <v-card-subtitle>Supervisores actuales</v-card-subtitle>
+                <v-card-text
+                  :class="['pa-2', 'black_selected']"
+                  sm="6"
+                  md="4"
+                  v-for="(item, index) in itemsSupervisorList"
+                  :key="index"
+                >
+                  {{ item.name }}
+                  <v-btn
+                    icon
+                    color="success"
+                    class="['mr-2','float-right']"
+                    small
+                    @click="changeFormSupervisor(index)"
+                  >
+                    <v-icon>mdi-pencil</v-icon>
+                  </v-btn>
+                  <v-btn
+                    icon
+                    color="primary"
+                    small
+                    @click="deleteSupervisor(item.id)"
+                  >
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                </v-card-text>
+              </v-card>
+            </v-form>
           </v-col>
         </v-row>
 
@@ -127,23 +135,102 @@
       </v-stepper-content>
 
       <v-stepper-step :complete="stepNumber > 4" step="4" editable
-        >Tipos de incidentes</v-stepper-step
+        >Nombres de incidentes</v-stepper-step
       >
       <v-stepper-content step="4">
         <v-row>
           <v-col cols="12" sm="6" md="4">
-            <v-combobox
+            <v-form @submit.prevent="addIncident" v-if="formAddIncident">
+              <v-text-field
+                v-model="textFieldIncident"
+                label="Agregar un nombre de incidente"
+                autocomplete="off"
+              ></v-text-field>
+              <v-btn color="success" outlined small type="submit"
+                >Agregar</v-btn
+              >
+            </v-form>
+            <v-form @submit.prevent="editIncident" v-if="!formAddIncident">
+              <v-text-field
+                v-model="textFieldIncident"
+                label="Agregar un nombre de incidente"
+                autocomplete="off"
+              ></v-text-field>
+              <v-btn color="warning" outlined small type="submit">editar</v-btn>
+            </v-form>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card sm="6" md="4" width="400" left>
+              <v-card-subtitle
+                v-if="itemsIncidenteList.length == 0 ? true : false"
+                >No hay incidentes cargados actualmente</v-card-subtitle
+              >
+            </v-card>
+            <v-form v-if="itemsIncidenteList.length != 0 ? true : false">
+              <v-card sm="6" md="4" width="400" left>
+                <v-card-subtitle>Incidentes actuales</v-card-subtitle>
+                <v-card-text
+                  :class="['pa-2', 'black_selected']"
+                  sm="6"
+                  md="4"
+                  v-for="(item, index) in itemsIncidenteList"
+                  :key="index"
+                >
+                  {{ item.name }}
+                  <v-btn
+                    icon
+                    color="success"
+                    class="['mr-2','float-right']"
+                    small
+                    @click="changeFormIncident(index)"
+                  >
+                    <v-icon>mdi-pencil</v-icon>
+                  </v-btn>
+                  <v-btn
+                    icon
+                    color="primary"
+                    small
+                    @click="deleteIncident(item.id)"
+                  >
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                </v-card-text>
+              </v-card>
+            </v-form>
+          </v-col>
+        </v-row>
+
+        <v-btn color="primary" @click="increaseStep()">Continue</v-btn>
+        <v-btn
+          text
+          @click="clearComboBox"
+          outlined
+          color="grey_selected"
+          class="ml-2"
+          >Cancel</v-btn
+        >
+      </v-stepper-content>
+
+      <v-stepper-step :complete="stepNumber > 5" step="5" editable
+        >Tipos de incidentes</v-stepper-step
+      >
+      <v-stepper-content step="5">
+        <v-row>
+          <v-col cols="12" sm="6" md="4">
+            <v-select
               v-model="comboIncident"
-              :items="itemsIncidentTypeList"
+              :items="itemsIncidenteList"
               item-value="id"
               item-text="name"
               :return-object="true"
               :search-input.sync="search"
               hide-selected
               label="Agregar un tipo de incidente"
-              multiple
               persistent-hint
               chips
+              @change="IncidentSelected(comboIncident)"
               @input.native="course = $event.srcElement.value"
             >
               <template v-slot:no-data>
@@ -157,87 +244,133 @@
                   </v-list-item-content>
                 </v-list-item>
               </template>
-            </v-combobox>
+            </v-select>
           </v-col>
-
+        </v-row>
+        <v-row>
           <v-col cols="12" sm="6" md="4">
-            <v-combobox
-              v-model="comboIncidentType"
+            <v-form
+              @submit.prevent="addTypeIncident"
+              v-if="formAddTypeIncident"
+            >
+              <v-text-field
+                v-model="textFieldIncident"
+                label="Agregar un nombre al tipo de incidente"
+                autocomplete="off"
+              ></v-text-field>
+              <v-btn color="success" outlined small type="submit"
+                >Agregar</v-btn
+              >
+            </v-form>
+            <v-form
+              @submit.prevent="editTypeIncident"
+              v-if="!formAddTypeIncident"
+            >
+              <v-text-field
+                v-model="textFieldIncident"
+                label="Edotar el nombre del tipo incidente"
+                autocomplete="off"
+              ></v-text-field>
+              <v-btn color="warning" outlined small type="submit">editar</v-btn>
+            </v-form>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-form v-if="TypeListSelected.length != 0 ? true : false">
+              <v-card sm="6" md="4" width="400" left>
+                <v-card-subtitle>Tipos de incidente actuales</v-card-subtitle>
+                <v-card-text
+                  :class="['pa-2', 'black_selected']"
+                  sm="6"
+                  md="4"
+                  v-for="(item, index) in TypeListSelected"
+                  :key="index"
+                >
+                  {{ item.nameIncident }}
+                  <v-btn
+                    icon
+                    color="success"
+                    class="['mr-2','float-right']"
+                    small
+                    @click="changeFormSupervisor(index)"
+                  >
+                    <v-icon>mdi-pencil</v-icon>
+                  </v-btn>
+                  <v-btn
+                    icon
+                    color="primary"
+                    small
+                    @click="deleteSupervisor(item.id)"
+                  >
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                </v-card-text>
+              </v-card>
+            </v-form>
+          </v-col>
+        </v-row>
+        <v-btn color="primary" @click="increaseStep()">Continue</v-btn>
+        <v-btn
+          text
+          @click="clearComboBox"
+          outlined
+          color="grey_selected"
+          class="ml-2"
+          >Cancel</v-btn
+        >
+      </v-stepper-content>
+
+      <v-stepper-step :complete="stepNumber > 6" step="6" editable
+        >Tareas comunes de los incidentes</v-stepper-step
+      >
+      <v-stepper-content step="6">
+        <v-row>
+          <v-col cols="12" sm="6" md="4">
+            <v-select
+              v-model="comboIncident"
               :items="itemsIncidenteList"
               item-value="id"
               item-text="name"
               :return-object="true"
               :search-input.sync="search"
               hide-selected
-              label="Agregar un nombre de incidente"
-              multiple
+              label="Agregar un tipo de incidente"
               persistent-hint
               chips
+              @change="IncidentSelected(comboIncident)"
               @input.native="course = $event.srcElement.value"
             >
               <template v-slot:no-data>
                 <v-list-item>
                   <v-list-item-content>
                     <v-list-item-title>
-                      No existe el incidentes "
+                      No se encuentra el supervisor "
                       <strong>{{ search }}</strong
                       >". Presione <kbd>enter</kbd> para ingresarlo
                     </v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
               </template>
-            </v-combobox>
+            </v-select>
           </v-col>
-        </v-row>
-
-        <!--aqui-->
-
-        <tbody>
-          <div v-for="items in itemsIncidentTypeList" v-bind:key="items.id">
-            <div v-for="item in items.nameType" v-bind:key="item.id">
-              <span>{{ item.nameIncident }}</span>
-            </div>
-          </div>
-        </tbody>
-        <v-col cols="12">
-          <v-autocomplete
-            :items="itemsIncidentTypeList"
-            dense
-            chips
-            small-chips
-            label="Outlined"
-          ></v-autocomplete>
-        </v-col>
-
-        <v-row>
           <v-col cols="12" sm="6" md="4">
-            <v-combobox
-              v-model="comoboIncident"
-              :items="itemsIncidentTypeList"
+            <v-select
+              v-model="comboIncidentType"
+              :items="TypeListSelected"
+              item-value="id"
               item-text="nameIncident"
               :return-object="true"
               :search-input.sync="search"
               hide-selected
               label="Agregar un nombre de incidente"
-              multiple
               persistent-hint
               chips
               @input.native="course = $event.srcElement.value"
-            >
-              <template v-slot:no-data>
-                <v-list-item>
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      No existe el incidentes "
-                      <strong>{{ search }}</strong
-                      >". Presione <kbd>enter</kbd> para ingresarlo
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </template>
-            </v-combobox>
+            ></v-select>
           </v-col>
         </v-row>
+
         <!--tarjetas donde te perimite crear, editar o eliminar map points de un cierto incidente-->
         <v-layout>
           <v-flex md4 v-if="formDescriptionMapPoint">
@@ -329,11 +462,10 @@
           >Cancel</v-btn
         >
       </v-stepper-content>
-
-      <v-stepper-step :complete="stepNumber > 5" step="5" editable
+      <v-stepper-step :complete="stepNumber > 7" step="7" editable
         >Tipos de recursos</v-stepper-step
       >
-      <v-stepper-content step="5">
+      <v-stepper-content step="7">
         <v-row>
           <v-col cols="12" sm="6" md="4">
             <v-combobox
@@ -366,7 +498,6 @@
           <v-col cols="12" sm="6" md="4">
             <v-autocomplete
               v-model="AutoCompleteIncident"
-              :items="itemsIncidenteList"
               item-text="name"
               hide-selected
               label="seleccionar un incidente"
@@ -407,9 +538,13 @@ export default {
       snackbar: false,
       formDescriptionMapPoint: true,
       formAddSupervisor: true,
+      formAddIncident: true,
+      formAddTypeIncident: true,
       domainField: "",
       newMapDescription: "",
       textFieldSupervisor: "",
+      textFieldIncident: "",
+      TypeListSelected: [],
       AdminField: "",
       stepNumber: "1",
       comboSupervisor: "",
@@ -422,7 +557,7 @@ export default {
       ],
       comboIncidentType: "",
       comoboIncident: "",
-      itemsIncidentTypeList: [
+      itemsIncidenteList: [
         {
           id: 1,
           name: "Incendio",
@@ -432,18 +567,13 @@ export default {
             { nameIncident: "campo" }
           ]
         },
-        { id: 2, name: "Rastrillaje" },
+        { id: 2, name: "Rastrillaje", nameType: [] },
         {
           id: 3,
           name: "Rescate de felino domestico",
           nameType: [{ nameIncident: "arbol" }, { nameIncident: "chimenea" }]
         },
-        { id: 4, name: "Auxilio" }
-      ],
-      itemsIncidenteList: [
-        { id: 1, type: "Incendio", name: "Casa" },
-        { id: 2, type: "Incendio", name: "Edificio" },
-        { id: 3, type: "Incendio", name: "Campo" }
+        { id: 4, name: "Auxilio", nameType: [] }
       ],
       MapPointSelectedList: [
         {
@@ -589,6 +719,46 @@ export default {
         );
       }
     },
+    addIncident() {
+      if (this.textFieldIncident.trim().length === 0) {
+        this.messagge = "El campo de incidente esta vacío";
+        this.snackbar = true;
+        return;
+      }
+      var incidentIsRepeated = this.itemsIncidenteList.some(
+        e =>
+          e.name.toLowerCase().trim() ==
+          this.textFieldIncident.toLowerCase().trim()
+      );
+      if (incidentIsRepeated) {
+        this.messagge = "El incidente ingresado ya existe";
+        this.snackbar = true;
+        return;
+      }
+      this.itemsIncidenteList.push({
+        id: Date.now(),
+        name: this.textFieldIncident.trim(),
+        nameType: []
+      });
+      this.textFieldIncident = "";
+    },
+    deleteIncident(id) {
+      if (confirm("Desea elimninar este incidente?")) {
+        this.itemsIncidenteList = this.itemsIncidenteList.filter(
+          e => e.id != id
+        );
+      }
+    },
+    editIncident() {
+      this.itemsIncidenteList[this.IncidetnIndex].name = this.textFieldIncident;
+      this.textFieldIncident = "";
+      this.formAddIncident = true;
+    },
+    changeFormIncident(IncidetnIndex) {
+      this.formAddIncident = false;
+      this.textFieldIncident = this.itemsIncidenteList[IncidetnIndex].name;
+      this.IncidetnIndex = IncidetnIndex;
+    },
 
     addSupervisor() {
       if (this.textFieldSupervisor.trim().length === 0) {
@@ -646,6 +816,10 @@ export default {
         supervisorAliasIndex
       ].name;
       this.supervisorAliasIndex = supervisorAliasIndex;
+    },
+    IncidentSelected(valueSelected) {
+      this.TypeListSelected = "";
+      this.TypeListSelected = valueSelected.nameType;
     }
   }
 };
