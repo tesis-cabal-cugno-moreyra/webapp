24d07558d3e2721131bfffa63fc194794e150c00
Gnoq <emi.cugno@gmail.com>
2020-09-26 18:09:17 -0300

    Fix margin/padding error on Error view

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,18 +1,16 @@
 <template>
   <v-app>
-    <v-main>
-      <v-container fill-height fluid>
-        <v-layout row>
-          <v-row align="center" justify="center">
-            <v-container text-center>
-              <h1 style="font-size: 102px">{{ this.errorCode }}</h1>
-              <h2>{{ this.errorMessage }}</h2>
-              <v-btn v-on:click="goHome">Ir a la página principal</v-btn>
-            </v-container>
-          </v-row>
-        </v-layout>
-      </v-container>
-    </v-main>
+    <v-container fill-height fluid>
+      <v-layout row>
+        <v-row align="center" justify="center">
+          <v-container text-center>
+            <h1 style="font-size: 102px">{{ this.errorCode }}</h1>
+            <h2>{{ this.errorMessage }}</h2>
+            <v-btn v-on:click="goHome">Ir a la página principal</v-btn>
+          </v-container>
+        </v-row>
+      </v-layout>
+    </v-container>
   </v-app>
 </template>
 
