e00c6be163ddf3ac69c98fad9ab394616d58932c
Gnoq <emi.cugno@gmail.com>
2020-10-25 19:06:29 -0300

    Fix query of resources, on incident resource list

--- src/store/domain-config/actions.js
+++ src/store/domain-config/actions.js
@@ -103,7 +103,7 @@ export default {
     // eslint-disable-next-line no-async-promise-executor
     return new Promise(async (resolve, reject) => {
       try {
-        let urlSearch = `/api/v1/resources/?user__first_name=${payload.user__first_name}&user__last_name=${payload.user__last_name}&type__name=${payload.type__name}&page=${payload.page}`;
+        let urlSearch = `/api/v1/resources/?user__first_name=${payload.user__first_name}&user__last_name=${payload.user__last_name}&type__name=${payload.type__name}&user__is_active=true&page=${payload.page}`;
 
         return resolve(await api.get(urlSearch));
       } catch (e) {
