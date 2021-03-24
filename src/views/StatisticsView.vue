6e619aa2d78f9bca386758e67f79f964eb27b6a7
Gnoq <emi.cugno@gmail.com>
2021-03-24 20:20:30 -0300

    WIP statistics view

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -1,31 +1,32 @@
 <template>
   <v-app>
-    <v-container class="pt-4 mt-4" fill-height fill-width>
+    <v-container class="pt-4 mt-4" fill-height>
       <v-layout row>
-        <v-row align="center" justify="center">
-          <v-card>
-            <line-chart></line-chart>
+        <v-row align="center" justify="start">
+          <v-card class="d-inline xs-1 sm-1 md-1 lg-1 xl-1">
+            <v-card-title
+              >Cantidad total de participaciones en incidentes</v-card-title
+            >
+            <v-card-text align="center" justify="center"
+              ><h1>12</h1></v-card-text
+            >
           </v-card>
-          <v-card>
+          <v-card class="d-inline">
+            <v-card-title
+              >Cantidad de incidentes por mes, por día y por año.</v-card-title
+            >
+            <v-card-text>
+              <line-chart></line-chart>
+            </v-card-text>
+          </v-card>
+          <v-card class="d-inline">
             <pie-chart></pie-chart>
           </v-card>
-          <v-card>
+          <v-card class="d-inline">
             <bar-chart></bar-chart>
           </v-card>
-          <v-card>
+          <v-card class="d-inline">
             <doughnut-chart></doughnut-chart>
-          </v-card>
-          <v-card>
-            <line-chart></line-chart>
-          </v-card>
-          <v-card>
-            <line-chart></line-chart>
-          </v-card>
-          <v-card>
-            <line-chart></line-chart>
-          </v-card>
-          <v-card>
-            <line-chart></line-chart>
           </v-card>
         </v-row>
       </v-layout>
