485ff87d2cadc0bed404cd7ea7cd2ad8e1769f74
cabaljoaquin <cabaljoa@gmail.com>
2021-03-29 15:10:57 -0300

    Se valido las opciones para los administradores, y se mejoro visualmente el cartel de log out

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -22,7 +22,11 @@
         >
       </v-list-item>
 
-      <v-list-item link v-on:click="goDomainAccessCode">
+      <v-list-item
+        link
+        v-on:click="goDomainAccessCode"
+        v-show="isAdminPermission"
+      >
         <v-list-item-icon>
           <v-icon color="grey darken-1">mdi-settings</v-icon>
         </v-list-item-icon>
@@ -40,7 +44,7 @@
         >
       </v-list-item>
 
-      <v-list-group no-action sub-group>
+      <v-list-group no-action sub-group v-show="isAdminPermission">
         <template v-slot:activator>
           <v-list-item-content>
             <v-list-item-title>Administrar cuentas</v-list-item-title>
@@ -74,7 +78,7 @@
           >
         </v-list-item>
       </v-list-group>
-      <v-list-item link v-on:click="goUserManager">
+      <v-list-item link v-on:click="goUserManager" v-show="isAdminPermission">
         <v-list-item-icon>
           <v-icon color="grey darken-1">mdi-account-card-details</v-icon>
         </v-list-item-icon>
@@ -125,14 +129,25 @@ export default {
     return {
       drawer: false,
       user: null,
-      logoutModal: false
+      logoutModal: false,
+      isAdminPermission: false
     };
   },
 
   created() {
     this.user = authServices.getUser();
+
+    this.administrationPermissionToSee();
   },
   methods: {
+    administrationPermissionToSee() {
+      const userRoles = this.user.roles;
+      userRoles.forEach(role => {
+        if (role === "Administrator") {
+          this.isAdminPermission = true;
+        }
+      });
+    },
     openLogoutModal: function() {
       this.$emit("open-logout-modal");
     },
