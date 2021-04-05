81b1a2fafd6f3ad51e1d128272475c21e732bf4e
Gnoq <emi.cugno@gmail.com>
2021-04-05 19:12:01 -0300

    Fix Linechart module, before use it on LineChart component

--- src/components/charts/LineChart.js
+++ src/components/charts/LineChart.js
@@ -3,8 +3,35 @@ const { reactiveProp } = mixins;
 
 export default {
   extends: Line,
+  data: () => ({
+    options: {
+      responsive: true,
+      legend: {
+        labels: {
+          fontColor: "white"
+        }
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              fontColor: "white",
+              beginAtZero: true
+            }
+          }
+        ],
+        xAxes: [
+          {
+            ticks: {
+              fontColor: "white",
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  }),
   mixins: [reactiveProp],
-  props: ["options"],
   mounted() {
     // this.chartData is created in the mixin.
     // If you want to pass options please create a local options object
